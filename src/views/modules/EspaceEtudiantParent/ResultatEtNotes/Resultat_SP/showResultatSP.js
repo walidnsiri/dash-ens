2fde32aee46e2a6d58b1b25ef323b617c294b65b
walid nsiri <walid.nsiri@esprit.tn>
2021-06-03 17:29:12 +0100

    removed unnecessary files

--- src/views/modules/EspaceEtudiantParent/ResultatEtNotes/Resultat_SP/showResultatSP.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const showResultatSP = () => {
-
-    return (<></>)
-
-}
-
-export default showResultatSP
--- src/views/modules/EspaceEtudiantParent/ResultatEtNotes/Resultat_SR/showResultatSR.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const showResultatSR = () => {
-
-    return (<></>)
-
-}
-
-export default showResultatSR
